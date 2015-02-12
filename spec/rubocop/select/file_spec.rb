22eeea726fb5183698cb444dc43c0ab92e0dff44
sanemat <o.gata.ken@gmail.com>
2015-02-13 02:50:09 +0900

    test(file): add actual test

--- spec/rubocop/select/file_spec.rb
+++ spec/rubocop/select/file_spec.rb
@@ -31,4 +31,23 @@ describe RuboCop::Select::File do
              )).to eq(hit_one_file)
     end
   end
+  context 'actual example' do
+    let(:target_files) { ['.rubocop.yml', 'app/jobs/repo_synchronization_job.rb'] }
+    let(:target_dir) { '/path/to' }
+    let(:compare_files) { [
+      '/path/to/bundle_bin/sprockets',
+      '/path/to/app/jobs/repo_synchronization_job.rb',
+      '/path/to/Rakefile'
+    ] }
+    let(:compare_dir) { '/woo' }
+    let(:hit_one_file) { [(Pathname.new '/path/to/app/jobs/repo_synchronization_job.rb')] }
+    it 'returns one intersection' do
+      expect(described_class.intersect(
+               target_files,
+               target_dir,
+               compare_files,
+               compare_dir
+             )).to eq(hit_one_file)
+    end
+  end
 end
