2c01b55a1e58edcf3975756ded28e4ca4815eb35
sanemat <o.gata.ken@gmail.com>
2015-02-13 02:40:35 +0900

    feat(file): implement using File.expand_path

--- lib/rubocop/select/file.rb
+++ lib/rubocop/select/file.rb
@@ -3,17 +3,18 @@ module RuboCop
   module Select
     class File
       def self.intersect(
+        # TODO: I don't want to access actual file system
         before_files = [],
           before_base_dir = Dir.pwd,
           after_files = [],
           after_base_dir = Dir.pwd
       )
         before_files.map do |file|
-          Pathname.new(file).relative_path_from(Pathname.new(before_base_dir))
+          Pathname.new(::File.expand_path(file, before_base_dir))
         end \
         & \
           after_files.map do |file|
-            Pathname.new(file).relative_path_from(Pathname.new(after_base_dir))
+            Pathname.new(::File.expand_path(file, after_base_dir))
           end
       end
     end
--- spec/rubocop/select/file_spec.rb
+++ spec/rubocop/select/file_spec.rb
@@ -1,17 +1,34 @@
 require 'spec_helper'
 
 describe RuboCop::Select::File do
-  let(:target_files) { ['foo'] }
-  let(:target_dir) { 'baz' }
-  let(:compare_files) { ['baz'] }
-  let(:compare_dir) { 'goo' }
-  let(:empty_files) { [] }
-  it 'returns no intersection' do
-    expect(described_class.intersect(
-             target_files,
-             target_dir,
-             compare_files,
-             compare_dir
-           )).to eq(empty_files)
+  context 'random files' do
+    let(:target_files) { ['foo'] }
+    let(:target_dir) { 'baz' }
+    let(:compare_files) { ['baz'] }
+    let(:compare_dir) { 'goo' }
+    let(:empty_files) { [] }
+    it 'returns no intersection' do
+      expect(described_class.intersect(
+               target_files,
+               target_dir,
+               compare_files,
+               compare_dir
+             )).to eq(empty_files)
+    end
+  end
+  context 'hit one file' do
+    let(:target_files) { ['foo', 'bar'] }
+    let(:target_dir) { '/woo' }
+    let(:compare_files) { ['bar', 'baz'] }
+    let(:compare_dir) { '/woo' }
+    let(:hit_one_file) { [(Pathname.new '/woo/bar')] }
+    it 'returns one intersection' do
+      expect(described_class.intersect(
+               target_files,
+               target_dir,
+               compare_files,
+               compare_dir
+             )).to eq(hit_one_file)
+    end
   end
 end
