b098412a0a42849c3045bf502913b796bdee9ae2
sanemat <o.gata.ken@gmail.com>
2015-02-12 23:08:53 +0900

    docs(example): add example

--- example/simple.rb
+++ example/simple.rb
@@ -1,5 +1,18 @@
+require 'rubocop/select'
+require 'rubocop'
 
-full_scan_files = []
-selector = Rubocop::Select.new(full_scan_files)
-diff_files = []
+# use same option as rubocop
+config_store = RuboCop::ConfigStore.new
+target_finder = RuboCop::TargetFinder.new(config_store)
+full_scan_files = target_finder.find(paths)
+
+selector = RuboCop::Select.new(full_scan_files)
+diff_files = [
+  '.rubocop.yml',
+  'app/jobs/repo_synchronization_job.rb'
+]
+# full_scan_files
+# .map {|f| Pathname.new File.expand_path(f)} &\
+# diff_files
+# .map{|f| Pathname.new File.expand_path(f)}
 selector.intersect(diff_files) #=> target_files
