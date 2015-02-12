395bcf68cb2c487aa872c8f70cd22f413ad11dfa
sanemat <o.gata.ken@gmail.com>
2015-02-13 02:23:43 +0900

    chore(file): add meanful name

--- spec/rubocop/select/file_spec.rb
+++ spec/rubocop/select/file_spec.rb
@@ -1,7 +1,17 @@
 require 'spec_helper'
 
 describe RuboCop::Select::File do
+  let(:target_files) { ['foo'] }
+  let(:target_dir) { 'baz' }
+  let(:compare_files) { ['baz'] }
+  let(:compare_dir) { 'goo' }
+  let(:empty_files) { [] }
   it 'returns no intersection' do
-    expect(described_class.intersect(['foo'], 'bar', ['baz'], 'goo')).to eq([])
+    expect(described_class.intersect(
+             target_files,
+             target_dir,
+             compare_files,
+             compare_dir
+           )).to eq(empty_files)
   end
 end
